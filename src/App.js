13197d18da005a907a6d4d48cd2e4a2be6713c3f
koder <haroldc2005@gmail.com>
2021-11-07 13:19:02 -0500

    Koder

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import './App.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Beginning } from './components/beginning';
+import $ from 'jquery';
 
 function App() {
-  
   return(
     <Beginning/>
   );
--- src/components/beginning.js
+++ src/components/beginning.js
@@ -1,19 +1,41 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import fire from '../img/fire.png';
+import $ from 'jquery';
+import { Home } from './home';
+
 
 export class Beginning extends Component{
+	state = {
+		count: 8
+	}
+	componentDidMount(){
+		setInterval(()=>{
+			if(this.state.count != 0)
+			  this.setState({count: this.state.count-1});
+		},1200)
+	}
+	componentDidUpdate(){
+		
+	}
 	render(){
+		if(this.state.count != 0){
 		return(
 			<div className="App">
                <header className="App-header">
                  <img src={fire} className="App-logo" alt="logo" />
                  <p>
-                   WELCOME TO KODERX SALES.
+                   WELCOME TO KODERX SALES {this.state.count}
                  </p>
                  <a className="App-link" href="https://briefcase-coderx.herokuapp.com/" target="_blank" rel="noopener noreferrer">Briefcase Coderx</a>
                </header>
            </div>
 		)
+		}
+	   else{
+		 return(
+			 <Home/>
+		 )
+	   }
 	}
 }
