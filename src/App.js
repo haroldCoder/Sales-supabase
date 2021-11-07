9e9975e1d9fa0a26edbd112d0d078f71719a20b9
koder <haroldc2005@gmail.com>
2021-11-07 12:16:20 -0500

    koder

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Beginning } from './components/beginning';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src="https://th.bing.com/th/id/OIP.kNzXOLq4BUkic1tJUN9QSQHaHa?pid=ImgDet&rs=1" className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
-      </header>
-    </div>
+  
+  return(
+    <Beginning/>
   );
 }
 
