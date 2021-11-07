e7280498c5fb43ae6ba7af2a9fb17f01829576a0
koder <haroldc2005@gmail.com>
2021-11-07 11:35:57 -0500

    Koder

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://th.bing.com/th/id/OIP.kNzXOLq4BUkic1tJUN9QSQHaHa?pid=ImgDet&rs=1" className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
