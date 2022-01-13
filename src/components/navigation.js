7ba4dca16cb76e65d50aab7b1face72c66d48788
koder <haroldc2005@gmail.com>
2022-01-13 16:04:31 -0500

    loder13

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
-import Example from './example';
 import {Link} from 'react-router-dom';
 import Access from '../layout/access.js';
 import Cookies from 'universal-cookie';
