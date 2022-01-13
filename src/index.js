83e5891a165476c628216af9b0dff58912964e22
koder <haroldc2005@gmail.com>
2022-01-13 10:45:36 -0500

    koder

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import {Beginning} from './components/beginning.js';
 import 'bootswatch/dist/morph/bootstrap.min.css';
 
