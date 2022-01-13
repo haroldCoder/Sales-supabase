154d6b87d5174bae2d9a76f0a300a638d9cbba28
koder <haroldc2005@gmail.com>
2022-01-13 15:57:41 -0500

    koder12

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import $ from 'jquery';
 import {BrowserRouter as Router,Route,Routes} from 'react-router-dom';
 import Home from './components/home';
