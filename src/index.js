17693fdd5ef6807da609c5b7c6efc871910ca105
koder <haroldc2005@gmail.com>
2022-01-13 10:59:33 -0500

    koder3

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {Beginning} from './components/beginning.js';
-import 'bootswatch/dist/morph/bootstrap.min.css';
 
 ReactDOM.render(
-  <Beginning/>,
+  <h2>hello</h2>,
   document.getElementById('root')
 );
