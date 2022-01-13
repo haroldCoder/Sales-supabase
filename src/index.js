5b1a7dc7744ca89fbf644e822c9e954bb5dc3ccb
koder <haroldc2005@gmail.com>
2022-01-13 11:07:39 -0500

    koder5

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {Beginning} from './components/beginning.js';
 import 'bootswatch/dist/morph/bootstrap.min.css';
 
 ReactDOM.render(
-  <h2>Hello</h2>,
+  <h2>hello</h2>,
   document.getElementById('root')
 );
