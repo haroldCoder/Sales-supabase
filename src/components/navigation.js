1076c9a1be12ce5e5319fb376fd1fecedac1d26f
koder <haroldc2005@gmail.com>
2022-01-13 17:24:55 -0500

    Koder

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import $ from 'jquery';
 
 export class Navigation extends Component{
 	constructor(props){
@@ -15,5 +17,23 @@ export class Navigation extends Component{
 				</div>
           </nav>
 		)
+	}
+	Main = () =>{
+		$(".dropdown").on("click",function(){
+			$(".dropdown-menu").slideToggle(300);
+		})
+		$(".use").css("cursor","pointer");
+		$(".use").css("font-size","7vh");
+		$(".use").css("color","#FFF");
+		$(".use").css("margin-right","20px");
+		$(".item1").click(function() {
+			$(".submenu1").slideToggle(500);
+		});
+		$("#item").click(function() {
+			$("#submenu").slideToggle(500);
+		})
+		$(".item3").click(function() {
+			$(".submenu3").slideToggle(500);
+		})
 	}
 }
