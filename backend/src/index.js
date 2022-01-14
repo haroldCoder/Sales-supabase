9eae8d1f986a204b208e1639121dddbc4731a468
koder <haroldc2005@gmail.com>
2022-01-14 10:53:47 -0500

    koder
    Signed-off-by: koder <haroldc2005@gmail.com>

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,7 +5,7 @@ const bodyParser =  require('body-parser');
 const app  =  express();
 const cors = require('cors')
 const mongoose = require('mongoose');
-app.use(cors({origin: 'http://localhost:3000'}))
+app.use(cors())
 app.use(express.json({limit: '10tb'}));
 const url = 'mongodb+srv://manuel:12345@702.s3tgn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const port =  8000;
