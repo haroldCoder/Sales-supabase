fb1844476881e717c156b54e52cd57a42cf2c963
haroldCoder <haroldc2005@gmail.com>
2023-08-15 15:20:35 -0500

    rename import

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React, {useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import './App.css'
-import Beginning from './components/Beginning.js';
+import Beginning from './components/Beginning';
 import 'bootswatch/dist/cyborg/bootstrap.min.css';
 
 ReactDOM.render(
