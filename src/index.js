0aa0725e49a4d97ff36e90e063c7c490b1a3fd65
haroldCoder <haroldc2005@gmail.com>
2023-08-15 15:12:35 -0500

    import and export in index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React, {useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import './App.css'
-import Beginning from './components/Beginning';
+import Beginning from './components/Beginning.js';
 import 'bootswatch/dist/cyborg/bootstrap.min.css';
 
 ReactDOM.render(
